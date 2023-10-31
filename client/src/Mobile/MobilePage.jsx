cc39a7f0fbf6316d387dce89e03990d69bbc6a65
hannah <hanna0115@naver.com>
2023-10-31 16:03:26 +0900

    ✨ mobile page 경로 및 페이지 설정

---
+++ client/src/Mobile/MobilePage.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import MainPage from './Page/MainPage';
+import KategoriePage from './Page/KategoriePage';
+import ProductDetailsPage from './Page/ProductDetailsPage';
+import JoinPage from './Page/JoinPage';
+import LoginPage from './Page/LoginPage';
+import LeavePage from './Page/LeavePage';
+import CartPage from './Page/CartPage';
+import MyPage from './Page/MyPage';
+import MyInfoPage from './Page/MyInfoPage';
+import LikePage from './Page/LikePage';
+import SearchPage from './Page/SearchPage';
+import PaymentPage from './Page/PaymentPage';
+
+const MobilePage = () => {
+  return (
+    <div>
+      <BrowerRouter>
+        <Routes>
+          <Route path='/' element={<MainPage/>}/>
+          <Route path='Kategorie' element={<KategoriePage/>}/>
+          <Route path='ProductDetails' element={<ProductDetailsPage/>}/>
+          <Route path='Join' element={<JoinPage/>}/>
+          <Route path='Login' element={<LoginPage/>}/>
+          <Route path='Leave' element={<LeavePage/>}/>
+          <Route path='Cart' element={<CartPage/>}/>
+          <Route path='MyPage' element={<MyPage/>}/>
+          <Route path='MyInfo' element={<MyInfoPage/>}/>
+          <Route path='Like' element={<LikePage/>}/>
+          <Route path='Search' element={<SearchPage/>}/>
+          <Route path='Payment' element={<PaymentPage/>}/>
+        </Routes>
+      </BrowerRouter>
+    </div>
+  );
+};
+
+export default MobilePage;
---
+++ client/src/Mobile/Page/KategoriePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const KategoriePage = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default KategoriePage;
