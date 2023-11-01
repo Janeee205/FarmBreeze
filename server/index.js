d1b6612cbf9bebd40790194d5e3aeec12408a26b
Janeee205 <ga473625@gmail.com>
2023-11-01 21:49:25 +0900

    🚧 작업중 (mongoose 코드 추가)

--- server/index.js
+++ server/index.js
@@ -9,3 +9,34 @@ app.listen(7000, function () {
 });
 
 app.use(express.static(__dirname))
+
+// ************* mongoose *************
+const mongoose = require('mongoose');
+const port = 3000;
+
+// MongoDB 연결
+mongoose.connect('mongodb+srv://admin:qewr1324@cluster0.yb4lr5p.mongodb.net/?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// MongoDB 연결 확인
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB 연결 오류:'));
+db.once('open', () => {
+  console.log('MongoDB 연결 성공!');
+
+  // 'users' 콜렉션에 접근
+  const User = mongoose.model('User', new mongoose.Schema({ name: String, email: String }), 'user');
+
+  // 'users' 콜렉션에서 데이터 조회 (Promise를 사용)
+  User.find({}).then(users => {
+    console.log('조회된 사용자 데이터:', users);
+  }).catch(err => {
+    console.error('데이터 조회 오류:', err);
+  });
+});
+
+app.listen(port, () => {
+  console.log(`서버가 포트 ${port}에서 실행 중입니다.`);
+});
