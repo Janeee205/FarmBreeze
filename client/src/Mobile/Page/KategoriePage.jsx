a4f63baa90e98cc0b3760b5c3746a5333f472f9d
biabamroi@gmail.com <biabamroi@gmail.com>
2023-11-05 03:14:21 +0900

    🚧 기본 틀 잡기

--- client/src/Mobile/Page/KategoriePage.jsx
+++ client/src/Mobile/Page/KategoriePage.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Icon } from '@iconify/react';
 // import TabBar from 'components/TabBar';
 
 const KategoriePage = () => {
   return (
     <div>
-      
+      <div className='kategorie-header'>
+        <h2>전체 목록</h2>
+        <Icon icon="grommet-icons:cart" color="white" />
+      </div>
       {/* <TabBar/> */}
     </div>
   );
