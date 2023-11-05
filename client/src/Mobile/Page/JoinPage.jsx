2a2000abeb4183ca6e320a5b0f4f6074c0b0f524
Janeee205 <ga473625@gmail.com>
2023-11-05 16:21:51 +0900

    📝 회원가입 작업중

--- client/src/Mobile/Page/JoinPage.jsx
+++ client/src/Mobile/Page/JoinPage.jsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React from "react";
 
 const JoinPage = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <div className="join-wrapper">
+        <label htmlFor="">
+          <input type="text" name="id" id="id" />
+          <button type="button">중복확인</button>
+        </label>
+      </div>
+    </>
   );
 };
 
