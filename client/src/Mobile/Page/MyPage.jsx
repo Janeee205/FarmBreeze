b885ffdf8cdad859e18c88c995f16b00357049c7
hannah <hanna0115@naver.com>
2023-11-07 00:32:22 +0900

    ✨ TabBar 컴포넌트 연결

--- client/src/Mobile/Page/MyPage.jsx
+++ client/src/Mobile/Page/MyPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../Component/Header';
+import TabBar from '../Component/TabBar';
 
 const MyPage = () => {
   return (
     <div>
       <Header title='마이페이지'/>
+      <TabBar/>
     </div>
   );
 };
