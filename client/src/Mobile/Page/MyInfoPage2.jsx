3af0d798d88a33d196660c34943f09efe3ab4a47
Janeee205 <ga473625@gmail.com>
2023-11-06 22:54:05 +0900

    🚚

---
+++ client/src/Mobile/Page/MyInfoPage2.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MyInfoPage2 = () => {
+  return <div>떠라</div>;
+};
+
+export default MyInfoPage2;
--- client/src/Mobile/Page/MyInfoPage_Jueun.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const MyInfoPage_Jueun = () => {
-  return <div></div>;
-};
-
-export default MyInfoPage_Jueun;
