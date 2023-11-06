f83ec998fae1593b198f958735c4177ed0ca36d7
hannah <hanna0115@naver.com>
2023-11-07 00:32:49 +0900

    📝 TabBar 마크업 완료

--- client/src/Mobile/Component/TabBar.jsx
+++ client/src/Mobile/Component/TabBar.jsx
@@ -1,9 +1,51 @@
 import React from 'react';
+import { Link, useParams } from 'react-router-dom';
+import { Icon } from '@iconify/react';
+import './TabBar.css';
 
 const TabBar = () => {
+  const tabMenu = [
+    {
+      title : '카테고리',
+      icon : 'ci:hamburger-md',
+      to : '/Category'
+    },
+    {
+      title : '검색',
+      icon : 'tdesign:search',
+      to : '/Search'
+    },
+    {
+      title : '홈',
+      icon : 'material-symbols:logo-dev-outline',
+      to : '/'
+    },
+    {
+      title : '좋아요',
+      icon : 'tabler:heart',
+      to : '/Like'
+    },
+    {
+      title : '마이페이지',
+      icon : 'ion:person-outline',
+      to : '/MyPage'
+    }
+  ]
+
   return (
-    <div>
-      
+    <div className='tab-bar'>
+      {
+        tabMenu.map((item, index) => {
+          return(
+            <div className='tab-bar-item' key={index}>
+              <Link to={item.to}>
+                <Icon icon={item.icon} />
+              </Link>
+              <p>{item.title}</p>
+            </div>
+          )
+        })
+      }
     </div>
   );
 };
