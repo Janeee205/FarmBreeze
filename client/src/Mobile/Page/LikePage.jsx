ab7f950d1f4c82ee31b240d13fa14440ddc7f616
biabamroi@gmail.com <biabamroi@gmail.com>
2023-11-10 22:32:15 +0900

    ✨ Link to Icon add

--- client/src/Mobile/Page/LikePage.jsx
+++ client/src/Mobile/Page/LikePage.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Icon } from '@iconify/react';
 import Header from '../Component/Header.jsx';
 import TabBar from '../Component/TabBar.jsx';
 
@@ -6,6 +8,12 @@ const LikePage = () => {
   return (
     <div>
       <Header title='좋아요' />
+      <Link to="/Search" style={{ position: 'absolute', right: '4rem', top: '2rem' }}>
+        <Icon className='like-search-icon' icon="material-symbols:search" color="#5e9fc3" height='2rem' />
+      </Link>
+      <Link to="/Cart" style={{ position: 'absolute', right: '1rem', top: '2rem' }}>
+        <Icon className='like-cart' icon="ion:cart-outline" color="#005792" height='2rem' />
+      </Link>
       <TabBar/>
     </div>
   );
