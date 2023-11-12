221a16c98b0c6ec2c2e50816b1f486e63ec654dc
hannah <hanna0115@naver.com>
2023-11-13 00:14:38 +0900

    🚧 📝  mypage 마크업 중

--- client/src/Mobile/Page/MyPage.jsx
+++ client/src/Mobile/Page/MyPage.jsx
@@ -1,11 +1,48 @@
 import React from 'react';
 import Header from '../Component/Header';
 import TabBar from '../Component/TabBar';
+import MyPageComponent from '../Component/MyPageComponent';
 
 const MyPage = () => {
+  const order = [
+    {
+      title : '결재완료',
+      count : '0'
+    },
+    {
+      title : '상품준비중',
+      count : '0'
+    },
+    {
+      title : '배송중',
+      count : '0'
+    },
+    {
+      title : '배송완료',
+      count : '0'
+    }
+  ]
   return (
     <div>
       <Header title='마이페이지'/>
+      <div className="my-order-info">
+        <div className="order">
+          {
+            order.map((item, index) => {
+              return(
+                <div>
+                  <p>{item.count}</p>
+                  <p>{item.title}</p>
+                </div>
+              )
+            })
+          }
+        </div>
+        <div className="orderheck">
+          <MyPageComponent title='주문/배송 조회'/>
+          <MyPageComponent title='취소/반품/교환 조회'/>
+        </div>
+      </div>
       <TabBar/>
     </div>
   );
