5e91c311c2af9aa57484dabec8a62918c1b9adf5
hannah <hanna0115@naver.com>
2023-11-16 21:52:34 +0900

    📝 마이페이지 마크업 완료

--- client/src/Mobile/Page/MyPage.jsx
+++ client/src/Mobile/Page/MyPage.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Icon } from '@iconify/react';
 import Header from '../Component/Header';
 import TabBar from '../Component/TabBar';
 import MyPageComponent from '../Component/MyPageComponent';
@@ -22,10 +24,27 @@ const MyPage = () => {
       count : '0'
     }
   ]
+
+  const customerService = [
+    {
+      title : '고객센터',
+      to : ''
+    },
+    {
+      title : '로그아웃',
+      to : ''
+    },
+    {
+      title : '회원탈퇴',
+      to : '/Leave'
+    },
+  ]
+
   return (
     <div>
       <Header title='마이페이지'/>
       <div className="my-order-info">
+        <h3>나의 주문정보</h3>
         <div className="order">
           {
             order.map((item, index) => {
@@ -38,11 +57,46 @@ const MyPage = () => {
             })
           }
         </div>
-        <div className="orderheck">
+        
+        <div className="order-check">
+
           <MyPageComponent title='주문/배송 조회'/>
           <MyPageComponent title='취소/반품/교환 조회'/>
         </div>
       </div>
+
+      <div className="my-shopping-benefits">
+        <h3>나의 쇼핑혜택</h3>
+        <MyPageComponent title='쿠폰'/>
+        <MyPageComponent title='마일리지'/>
+      </div>
+
+      <div className="shopping-activities">
+        <h3>나의 쇼핑활동</h3>
+        <MyPageComponent title='나의 찜'/>
+        <MyPageComponent title='최근 본 상품'/>
+        <MyPageComponent title='상품리뷰'/>
+        <MyPageComponent title='상품문의'/>
+        <MyPageComponent title='1:1 문의내역'/>
+      </div>
+
+      <div className="my-Profile">
+      <h3>나의 회원정보</h3>
+        <MyPageComponent title='회원 정보 관리'/>
+        <MyPageComponent title='배송 정보 관리'/>
+        <MyPageComponent title='공지사항'/>
+      </div>
+
+      {
+        customerService.map((item, index) => {
+          return(
+            <Link className='customer-service'>
+              <p>{item.title}</p>
+              <Icon icon="iconamoon:arrow-right-2-light" style={{fontSize : '1.5rem'}}/>
+            </Link>
+          )
+        })
+      }
       <TabBar/>
     </div>
   );
