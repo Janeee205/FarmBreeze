fa9f3116b3d244664296dc0961f3e699db7fc876
hannah <hanna0115@naver.com>
2023-12-07 23:04:28 +0900

    📝 mainpage slide image 변경

--- client/src/Mobile/Component/MainSlide.jsx
+++ client/src/Mobile/Component/MainSlide.jsx
@@ -20,23 +20,23 @@ const MainSlide = () => {
   return (
     <Slider {...settings}>
       <div>
-        <img src="https://images.unsplash.com/photo-1596941248238-0d49dcaa4263?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+        <img src="/images/slide01.png" alt="" />
         <p>1/5</p>
       </div>
       <div>
-        <img src="https://images.unsplash.com/photo-1609766418204-94aae0ecfdfc?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+        <img src="/images/slide02.png" alt="" />
         <p>2/5</p>
       </div>
       <div>
-        <img src="https://images.unsplash.com/photo-1579169825453-8d4b4653cc2c?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+        <img src="/images/slide03.png" alt="" />
         <p>3/5</p>
       </div>
       <div>
-        <img src="https://images.unsplash.com/photo-1612977512598-3b8d6a498bbb?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+        <img src="/images/slide04.png" alt="" />
         <p>4/5</p>
       </div>
       <div>
-        <img src="https://images.unsplash.com/photo-1595737361672-ae84c6ca2298?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+        <img src="/images/slide05.png" alt="" />
         <p>5/5</p>
       </div>
     </Slider>
